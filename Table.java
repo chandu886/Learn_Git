1b1043843ed21ee7624c006c9d32391392f466f9
validName <gadudhula-chandu.gadudhula-chandu@capgemini.com>
2023-06-28 16:14:46 +0530

    Table and year

---
+++ Table.java
@@ -0,0 +1,13 @@
+public class Table {
+    public static void main(String args[]){
+        int n =10;
+        int product;
+        for(int i=1;i<=10;i++){
+           product =n*i;
+            System.out.println(n+ " * " +i+" = "+product);
+        }
+
+        
+    }
+    
+}
